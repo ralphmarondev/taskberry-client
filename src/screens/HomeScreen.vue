6a621cd684f7ecc9bfc38b9a56b389ce8e7a568a
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-06-23 10:18:02 +0800

    add new task modal

--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -4,7 +4,16 @@ export default {
     data() {
         return {
             tasks: [],
-            loading: true
+            loading: true,
+
+            showModal: false,
+            newTask: {
+                description: '',
+                completed: '',
+                priority: '',
+                start_time: '',
+                end_time: ''
+            }
         };
     },
     methods: {
@@ -15,6 +24,29 @@ export default {
                 this.loading = false;
             } catch (error) {
                 console.log(error);
+            }
+        },
+        // Modals
+        openModal() {
+            this.showModal = true;
+        },
+        closeModal() {
+            this.showModal = false;
+        },
+        async addTask() {
+            try {
+                let response = await this.$http.post('http://localhost:8000/api/tasks/', this.newTask);
+                this.tasks.push(response.data);
+                this.closeModal();
+                this.newTask = {
+                    description: '',
+                    completed: false,
+                    priority: 1,
+                    start_time: '',
+                    end_time: ''
+                }
+            } catch (error) {
+                console.log(`Error adding: ${error}`);
             }
         }
     },
@@ -29,34 +61,75 @@ export default {
     <div>
         <header>
             <div class="logo">Todo App</div>
-            <div class="action">New</div>
+            <div class="action" @click="openModal">New</div>
         </header>
 
         <main>
-
+            <table>
+                <thead>
+                    <tr>
+                        <th class="small-column">ID</th>
+                        <th class="large-column">Description</th>
+                        <th class="medium-column">Completed</th>
+                        <th class="medium-column">Priority</th>
+                        <th class="medium-column">Start Time</th>
+                        <th class="medium-column">End Time</th>
+                        <th class="medium-column">Update</th>
+                        <th class="medium-column">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="task in tasks" :key="task.id">
+                        <td class="small-column">{{ task.id }}</td>
+                        <td class="large-column">{{ task.description }}</td>
+                        <td class="medium-column">{{ task.completed }}</td>
+                        <td class="medium-column">{{ task.priority }}</td>
+                        <td class="medium-column">{{ task.start_time }}</td>
+                        <td class="medium-column">{{ task.end_time }}</td>
+                        <td class="medium-column"><button>Update</button></td>
+                        <td class="medium-column"><button>Delete</button></td>
+                    </tr>
+                </tbody>
+            </table>
         </main>
 
+        <!-- Modals -->
+        <div v-if="showModal" class="modal">
+            <div class="modal-content">
+                <span class="close" @click="closeModal">&times;</span>
+                <h2>Add New Task</h2>
 
-        <h1>Task List</h1>
-        <div v-if="loading">Loading...</div>
-        <div v-else>
-            <p v-if="tasks.length === 0">No tasks found.</p>
-            <div v-else>
-                <p v-for="task in tasks" :key="task.id">
-                    <strong>ID:</strong> {{ task.id }} <br>
-                    <strong>Description:</strong> {{ task.description }} <br>
-                    <strong>Completed:</strong> {{ task.completed ? 'Yes' : 'No' }} <br>
-                    <strong>Priority:</strong> {{ task.priority }} <br>
-                    <strong>Start Time:</strong> {{ task.start_time }} <br>
-                    <strong>End Time:</strong> {{ task.end_time }} <br>
-                    <br>
-                </p>
+                <form @submit.prevent="addTask">
+                    <label for="description">Description:</label>
+                    <input type="text" id="description" v-model="newTask.description" required>
+
+                    <label for="completed">Completed:</label>
+                    <input type="checkbox" id="completed" v-model="newTask.completed">
+
+                    <label for="priority">Priority:</label>
+                    <select id="priority" v-model="newTask.priority" required>
+                        <option value="1">Low</option>
+                        <option value="2">Medium</option>
+                        <option value="3">High</option>
+                    </select>
+
+                    <label for="start_time">Start Time:</label>
+                    <input type="datetime-local" id="start_time" v-model="newTask.start_time" required>
+
+                    <label for="end_time">End Time:</label>
+                    <input type="datetime-local" id="end_time" v-model="newTask.end_time" required>
+
+                    <button type="submit">Add Task</button>
+                </form>
             </div>
         </div>
+
     </div>
 </template>
 
 <style>
 @import url('css/style.css');
 @import url('css/header.css');
+@import url('css/main.css');
+@import url('css/modal.css');
 </style>
